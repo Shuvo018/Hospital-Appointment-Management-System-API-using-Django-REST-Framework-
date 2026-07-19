318da86c78af96dd17264f19456fc97bca39f49b
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-19 22:31:52 +0600

    feat: add logout functionality with token blacklisting

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from accounts.views import RegistrationView, LoginView
+from accounts.views import RegistrationView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegistrationView.as_view()),
     path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-
+from hospital_management.permissions import IsDoctorOnly, IsPatientOnly
 
 from accounts.serializers import RegisterSerializer, LoginSerializer
 
@@ -27,11 +27,23 @@ class RegistrationView(APIView):
         )
 
 class LoginView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
 
         if serializer.is_valid():
             print(serializer)
             return Response(serializer.validated_data)
-        print(serializer.errors)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LogoutView(APIView):
+    permission_classes = [IsPatientOnly, IsDoctorOnly]
+
+    def post(self, request):
+        try:
+            RefreshToken(request.data['refresh_token']).blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
